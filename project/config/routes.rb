29f9e295f9e54c42f827aa9641f6f343727d9098
miguelcanachila <rafael_andres_zamora@hotmail.com>
2018-07-26 20:45:40 -0500

    First Update Usuarios

--- project/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'usuarios/new'
-  #get 'welcome/index'
-  resources :usuarios
-  #post ':controller(/:action(/:id)(.:format)))'
-  #root :to => 'welcome#index'
-  root :to => 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
